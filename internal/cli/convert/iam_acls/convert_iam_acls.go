1b1f9e589d7dc6a62dda31b6af191c84423c09c6
Adrian Januzi <adrianjanuzi@hotmail.com>
2025-07-31 14:36:46 +0100

    Updating ACLs Map

--- internal/cli/convert/iam_acls/convert_iam_acls.go
+++ internal/cli/convert/iam_acls/convert_iam_acls.go
@@ -37,99 +37,82 @@ type TemplateData struct {
 type ACLMapping struct {
 	Operation       string
 	ResourceType    string
-	RequiresPattern bool
 }
 
+// https://docs.aws.amazon.com/service-authorization/latest/reference/list_apachekafkaapisforamazonmskclusters.html
+// https://docs.confluent.io/cloud/current/security/access-control/acl.html#acl-resources-and-operations-for-ccloud-summary
 var aclMap = map[string]ACLMapping{
 	"kafka-cluster:AlterCluster": {
 		Operation:    "Alter",
 		ResourceType: "Cluster",
-		RequiresPattern: false,
 	},
 	"kafka-cluster:AlterClusterDynamicConfiguration": {
 		Operation:    "AlterConfigs",
 		ResourceType: "Cluster",
-		RequiresPattern: false,
 	},
 	"kafka-cluster:AlterGroup": {
 		Operation:    "Read",
 		ResourceType: "Group",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:AlterTopic": {
 		Operation:    "Alter",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:AlterTopicDynamicConfiguration": {
 		Operation:    "AlterConfigs",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:AlterTransactionalId": {
 		Operation:    "Write",
 		ResourceType: "TransactionalId",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:CreateTopic": {
 		Operation:    "Create",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DeleteGroup": {
 		Operation:    "Delete",
 		ResourceType: "Group",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DeleteTopic": {
 		Operation:    "Delete",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DescribeCluster": {
 		Operation:    "Describe",
 		ResourceType: "Cluster",
-		RequiresPattern: false,
 	},
 	"kafka-cluster:DescribeClusterDynamicConfiguration": {
-		Operation:    "DescribeConfigs",
-		ResourceType: "Cluster",
-		RequiresPattern: false,
+		Operation:    "Describe", // OSK would map to DescribeConfigs.
+		ResourceType: "Cluster",		
 	},
 	"kafka-cluster:DescribeGroup": {
 		Operation:    "Describe",
 		ResourceType: "Group",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DescribeTopic": {
 		Operation:    "Describe",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DescribeTopicDynamicConfiguration": {
 		Operation:    "DescribeConfigs",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:DescribeTransactionalId": {
 		Operation:    "Describe",
 		ResourceType: "TransactionalId",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:ReadData": {
 		Operation:    "Read",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:WriteData": {
 		Operation:    "Write",
 		ResourceType: "Topic",
-		RequiresPattern: true,
 	},
 	"kafka-cluster:WriteDataIdempotently": {
 		Operation:    "IdempotentWrite",
 		ResourceType: "Cluster",
-		RequiresPattern: true,
 	},
 }
 
