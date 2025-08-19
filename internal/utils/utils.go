66e4cecabd10889d4b88bb8d89283be6cb502e03
Adrian Januzi <42680878+adrian-januzi@users.noreply.github.com>
2025-08-19 10:19:29 +0100

    fix(scan): kcp fails to connect to older Kafka clusters. (#44)

---
+++ internal/utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import "strings"
+
+func ConvertKafkaVersion(kafkaVersion *string) string {
+	switch {
+	case strings.Contains(*kafkaVersion, "kraft"):
+		return strings.ReplaceAll(*kafkaVersion, ".x.kraft", ".0")
+	case strings.Contains(*kafkaVersion, "x"):
+		return strings.ReplaceAll(*kafkaVersion, ".x", ".0")
+	case strings.Contains(*kafkaVersion, "tiered"):
+		return strings.ReplaceAll(*kafkaVersion, ".tiered", "")
+	case *kafkaVersion == "3.6.0.1":
+		return "3.6.0"
+	default:
+		return *kafkaVersion
+	}
+}
