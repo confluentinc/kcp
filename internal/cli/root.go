ae077e7f1211c58af07a25dc8c246774a19686bc
Ian Feeney <ifeeney@confluent.io>
2025-09-11 15:45:45 +0100

    Configuring logging to log to file as well as terminal (#99)

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -1,6 +1,11 @@
 package cli
 
 import (
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/confluentinc/kcp/internal/build_info"
 	"github.com/confluentinc/kcp/internal/cli/create_asset"
 	"github.com/confluentinc/kcp/internal/cli/discover"
 	i "github.com/confluentinc/kcp/internal/cli/init"
@@ -8,6 +13,7 @@ import (
 	"github.com/confluentinc/kcp/internal/cli/scan"
 	"github.com/confluentinc/kcp/internal/cli/version"
 	"github.com/spf13/cobra"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 var RootCmd = &cobra.Command{
@@ -17,6 +23,18 @@ var RootCmd = &cobra.Command{
 }
 
 func init() {
+
+	lumberjackLogger := &lumberjack.Logger{
+		Filename:   "kcp.log",
+		MaxSize:    25, 
+		Compress:   true,
+	}
+
+	logger := slog.New(slog.NewTextHandler(io.MultiWriter(lumberjackLogger, os.Stdout), nil))
+	slog.SetDefault(logger)
+
+	slog.Info("Initializing KCP", "version", build_info.Version, "commit", build_info.Commit, "date", build_info.Date)
+
 	RootCmd.AddCommand(
 		i.NewInitCmd(),
 		create_asset.NewCreateAssetCmd(),
