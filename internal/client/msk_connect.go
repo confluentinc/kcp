9f809243f7cdfbeb8c35139d096b4a85565cde4c
Adrian Januzi <adrianjanuzi@hotmail.com>
2025-09-19 10:25:12 +0100

    msk-connect service/client

---
+++ internal/client/msk_connect.go
@@ -0,0 +1,22 @@
+package client
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/kafkaconnect"
+)
+
+func NewMSKConnectClient(region string) (*kafkaconnect.Client, error) {
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return nil, fmt.Errorf("❌ Failed to load AWS config: %v", err)
+	}
+
+	if region != "" {
+		cfg.Region = region
+	}
+
+	return kafkaconnect.NewFromConfig(cfg), nil
+}
