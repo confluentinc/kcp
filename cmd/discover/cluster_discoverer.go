f3fa48552233aa071b22ea3e69494e00027d37f4
Glenn Smith <gsmith@confluent.io>
2025-10-01 09:18:19 +0100

    remove print

--- cmd/discover/cluster_discoverer.go
+++ cmd/discover/cluster_discoverer.go
@@ -361,7 +361,6 @@ func (cd *ClusterDiscoverer) discoverMetrics(ctx context.Context, clusterArn str
 	previousMidnight := time.Date(now.Year(), now.Month(), now.Day()-1, 0, 0, 0, 0, time.UTC)
 	endTime := previousMidnight.Add(24 * time.Hour)
 	timeWindow, err := metrics.GetTimeWindow(endTime, metrics.LastYear)
-	fmt.Printf("timeWindow: %+v\n", timeWindow)
 	if err != nil {
 		return nil, fmt.Errorf("failed to calculate time window: %v", err)
 	}
