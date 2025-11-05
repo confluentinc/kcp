920499102fe68e4f1fe3c468c48bce6b9d91c1fc
Glenn Smith <gsmith@confluent.io>
2025-11-05 13:16:50 +0000

    wip

--- internal/services/hcl/aws/nat_gateway.go
+++ internal/services/hcl/aws/nat_gateway.go
@@ -1,14 +1,14 @@
 package aws
 
 import (
+	"github.com/confluentinc/kcp/internal/utils"
 	"github.com/hashicorp/hcl/v2/hclwrite"
-	"github.com/zclconf/go-cty/cty"
 )
 
 func GenerateNATGatewayResource(tfResourceName, allocationId, subnetId string) *hclwrite.Block {
 	natGatewayBlock := hclwrite.NewBlock("resource", []string{"aws_nat_gateway", tfResourceName})
-	natGatewayBlock.Body().SetAttributeValue("allocation_id", cty.StringVal(allocationId))
-	natGatewayBlock.Body().SetAttributeValue("subnet_id", cty.StringVal(subnetId))
+	natGatewayBlock.Body().SetAttributeRaw("allocation_id", utils.TokensForResourceReference(allocationId))
+	natGatewayBlock.Body().SetAttributeRaw("subnet_id", utils.TokensForResourceReference(subnetId))
 	natGatewayBlock.Body().AppendNewline()
 
 	return natGatewayBlock
--- internal/services/hcl/aws/route_table.go
+++ internal/services/hcl/aws/route_table.go
@@ -9,7 +9,11 @@ import (
 func GenerateRouteTableResource(tfResourceName, vpcId, gatewayIdRef string) *hclwrite.Block {
 	routeTableBlock := hclwrite.NewBlock("resource", []string{"aws_route_table", tfResourceName})
 	routeTableBlock.Body().SetAttributeValue("vpc_id", cty.StringVal(vpcId))
-	routeTableBlock.Body().SetAttributeRaw("gateway_id", utils.TokensForResourceReference(gatewayIdRef))
+	routeBlock := hclwrite.NewBlock("route", nil)
+	routeBlock.Body().SetAttributeValue("cidr_block", cty.StringVal("0.0.0.0/0"))
+	routeBlock.Body().SetAttributeRaw("gateway_id", utils.TokensForResourceReference(gatewayIdRef))
+	routeTableBlock.Body().AppendBlock(routeBlock)
+
 	routeTableBlock.Body().AppendNewline()
 
 	return routeTableBlock
