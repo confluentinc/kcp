f2c2e9255b9aae252d710696cd8ddacdb0d08a15
Glenn Smith <gsmith@confluent.io>
2026-02-02 14:23:29 +0000

    not spread opts

--- cmd/migration/init/migration_init.go
+++ cmd/migration/init/migration_init.go
@@ -30,72 +30,42 @@ type MigrationInitOpts struct {
 }
 
 type MigrationInit struct {
-	stateFile string
-
-	gatewayNamespace     string
-	gatewayCrdName       string
-	sourceName           string
-	destinationName      string
-	sourceRouteName      string
-	destinationRouteName string
-	kubeConfigPath       string
-
-	clusterId           string
-	clusterRestEndpoint string
-	clusterLinkName     string
-	clusterApiKey       string
-	clusterApiSecret    string
-	topics              []string
-	authMode            string
+	opts MigrationInitOpts
 }
 
 func NewMigrationInit(opts MigrationInitOpts) *MigrationInit {
 	return &MigrationInit{
-		stateFile:            opts.stateFile,
-		gatewayNamespace:     opts.gatewayNamespace,
-		gatewayCrdName:       opts.gatewayCrdName,
-		sourceName:           opts.sourceName,
-		destinationName:      opts.destinationName,
-		sourceRouteName:      opts.sourceRouteName,
-		destinationRouteName: opts.destinationRouteName,
-		kubeConfigPath:       opts.kubeConfigPath,
-		clusterId:            opts.clusterId,
-		clusterRestEndpoint:  opts.clusterRestEndpoint,
-		clusterLinkName:      opts.clusterLinkName,
-		clusterApiKey:        opts.clusterApiKey,
-		clusterApiSecret:     opts.clusterApiSecret,
-		topics:               opts.topics,
-		authMode:             opts.authMode,
+		opts: opts,
 	}
 }
 
 func (m *MigrationInit) Run() error {
 	migrationOpts := types.MigrationOpts{
-		GatewayNamespace:     m.gatewayNamespace,
-		GatewayCrdName:       m.gatewayCrdName,
-		SourceName:           m.sourceName,
-		DestinationName:      m.destinationName,
-		SourceRouteName:      m.sourceRouteName,
-		DestinationRouteName: m.destinationRouteName,
-		KubeConfigPath:       m.kubeConfigPath,
-		ClusterId:            m.clusterId,
-		ClusterRestEndpoint:  m.clusterRestEndpoint,
-		ClusterLinkName:      m.clusterLinkName,
-		Topics:               m.topics,
-		AuthMode:             m.authMode,
-		ClusterApiKey:        m.clusterApiKey,
-		ClusterApiSecret:     m.clusterApiSecret,
+		GatewayNamespace:     m.opts.gatewayNamespace,
+		GatewayCrdName:       m.opts.gatewayCrdName,
+		SourceName:           m.opts.sourceName,
+		DestinationName:      m.opts.destinationName,
+		SourceRouteName:      m.opts.sourceRouteName,
+		DestinationRouteName: m.opts.destinationRouteName,
+		KubeConfigPath:       m.opts.kubeConfigPath,
+		ClusterId:            m.opts.clusterId,
+		ClusterRestEndpoint:  m.opts.clusterRestEndpoint,
+		ClusterLinkName:      m.opts.clusterLinkName,
+		Topics:               m.opts.topics,
+		AuthMode:             m.opts.authMode,
+		ClusterApiKey:        m.opts.clusterApiKey,
+		ClusterApiSecret:     m.opts.clusterApiSecret,
 	}
 
 	migrationId := fmt.Sprintf("migration-%s", time.Now().Format("20060102-150405"))
-	migration := types.NewMigration(migrationId, m.stateFile, migrationOpts)
-	
+	migration := types.NewMigration(migrationId, m.opts.stateFile, migrationOpts)
+
 	if err := migration.Initialize(context.Background()); err != nil {
 		return fmt.Errorf("failed to initialize migration: %w", err)
 	}
-	
-	slog.Info("migration initialized", 
-		"migrationId", migration.MigrationId, 
+
+	slog.Info("migration initialized",
+		"migrationId", migration.MigrationId,
 		"currentState", migration.GetCurrentState())
 	return nil
 }
